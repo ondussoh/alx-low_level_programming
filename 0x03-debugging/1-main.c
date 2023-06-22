d54e60566b89c36e8357aad804d91b6cf5a23160
ondussoh <jacobookinyi@gmail.com>
2023-06-22 02:17:51 -0700

    Debugging corrections

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -9,12 +10,14 @@
 int main(void)
 {
 int i;
+ 
 printf("Infinite loop incoming :(\n");
 i = 0;
 /*
 *while (i < 10)
 *    putchar(i);
 */
+ 
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
