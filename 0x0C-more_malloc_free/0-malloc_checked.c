17e520e390d7a6b1bc99cba5f2bb7505f85a5464
ondussoh <jacobookinyi@gmail.com>
2023-07-13 03:14:27 -0700

    More memory allocations and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b) {
+    void *ptr = malloc(b);
+
+    if (ptr == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(98);
+    }
+
+    return ptr;
+}
