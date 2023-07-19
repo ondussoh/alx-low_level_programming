7d425230d4d6974bbf369b93dc4a4f1ef8d2ac22
ondussoh <jacobookinyi@gmail.com>
2023-07-19 02:45:31 -0700

    Pointers to Functions

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(int num_bytes) {
-    unsigned char* ptr = (unsigned char*)print_opcodes;
-    int i;
-    for (i = 0; i < num_bytes; i++) {
-        printf("%02x", ptr[i]);
-    }
-    printf("\n");
-}
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, nbytes;
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    int num_bytes = atoi(argv[1]);
+	nbytes = atoi(argv[1]);
 
-    if (num_bytes <= 0) {
-        printf("Error\n");
-        return 2;
-    }
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    print_opcodes(num_bytes);
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
 
-    return 0;
+	printf("\n");
+	return (0);
 }
