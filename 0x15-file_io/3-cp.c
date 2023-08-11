48dc09751391b04a33e87232f3c5a24a03709ba1
ondussoh <jacobookinyi@gmail.com>
2023-08-11 12:39:33 -0700

    File descriptors and permissions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,92 +1,72 @@
 #include "main.h"
 #include <stdio.h>
-char *create_buffer(void);
-void close_fd(int fd);
-/**
-  * close_fd-  prints error when cannot close fd
-  * @fd: fd unable to close
-  * Return: nothing
-  */
-void close_fd(int fd)
-{
-	int a = close(fd);
 
-	if (a == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
 /**
-  * main - copies from one file to another
-  * @argc: argument count
-  * @argv: arguments
-  * Return: Always 0
-  * Description:
-  * exit code 97 - argument count incorrect
-  * exit code 98 - file_from doesn't exist or cannot be read
-  * exit code 99 - file_to cannot be created or writted to
-  * exit code 100 - file cannot be closed
-  */
-int main(int argc, char *argv[])
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
 {
-	int fd1, fd2, red, wr;
-	char *temp_s;
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0774);
-	temp_s = create_buffer();
-	red = read(fd1, temp_s, 1024);
-	for (; red > 0;)
+	if (file_to == -1)
 	{
-		if (fd1 == -1 || red == -1)
-		{	dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", argv[1]);
-			free(temp_s);
-			exit(98);
-		}
-
-		wr = write(fd2, temp_s, red);
-		if (fd2 == -1 || wr == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(temp_s);
-			exit(99);
-		}
-		red = read(fd1, temp_s, 1024);
-		fd2 = open(argv[2], O_WRONLY | O_APPEND);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
-	free(temp_s);
-	close_fd(fd1);
-	close_fd(fd2);
-	return (0);
 }
 
 /**
-  * create_buffer- creates buffer and allocates 1024 bytes
-  *
-  * Return: returns buffer
-  */
-char *create_buffer()
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
-	char *store;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
-
-	store = malloc(sizeof(char) * 1024);
-
-	if (store == NULL)
+	if (argc != 3)
 	{
-		return (NULL);
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
-	return (store);
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
